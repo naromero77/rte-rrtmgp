00371ce77217d5387fd877613e99caf0d21697a2
Nichols A. Romero <coe0162@tulip.cm.cluster>
2020-02-28 22:54:17 -0600

    Atmos pointer does not need to be mapped.

--- rte/mo_rte_sw.F90
+++ rte/mo_rte_sw.F90
@@ -155,7 +155,7 @@ contains
         !
         ! Direct beam only
         !
-        !$omp target enter data map(to:atmos,atmos%tau)
+        !$omp target enter data map(to:atmos%tau)
         error_msg =  atmos%validate()
         if(len_trim(error_msg) > 0) return
         call sw_solver_noscat(ncol, nlay, ngpt, logical(top_at_1, wl), &
@@ -166,19 +166,19 @@ contains
         !
         !gpt_flux_up = 0._wp
         !gpt_flux_dn = 0._wp
-        !$omp target exit data map(release:atmos%tau,atmos)
+        !$omp target exit data map(release:atmos%tau)
       class is (ty_optical_props_2str)
         !
         ! two-stream calculation with scattering
         !
-        !$omp target enter data map(to:atmos,atmos%tau,atmos%ssa,atmos%g)
+        !$omp target enter data map(to:atmos%tau,atmos%ssa,atmos%g)
         error_msg =  atmos%validate()
         if(len_trim(error_msg) > 0) return
         call sw_solver_2stream(ncol, nlay, ngpt, logical(top_at_1, wl), &
                                atmos%tau, atmos%ssa, atmos%g, mu0,      &
                                sfc_alb_dir_gpt, sfc_alb_dif_gpt,        &
                                gpt_flux_up, gpt_flux_dn, gpt_flux_dir)
-        !$omp target exit data map(release:atmos%tau,atmos%ssa,atmos%g,atmos)
+        !$omp target exit data map(release:atmos%tau,atmos%ssa,atmos%g)
         !$omp target exit data map(release:sfc_alb_dir_gpt,sfc_alb_dif_gpt)
       class is (ty_optical_props_nstr)
         !
