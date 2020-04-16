880741437b82e79ea26286ea7cf2be63624574fb
naromero77 <naromero@anl.gov>
2020-04-15 22:29:06 -0500

    Eliminate omp map clauses on pointers.

--- rrtmgp/mo_gas_optics_rrtmgp.F90
+++ rrtmgp/mo_gas_optics_rrtmgp.F90
@@ -568,7 +568,6 @@ contains
     !$acc enter data create(col_mix, fminor)
     !$omp target enter data map(alloc:col_mix, fminor)
     !$acc enter data copyin(this)
-    !$omp target enter data map(to:this)
     !$acc enter data copyin(this%gpoint_flavor)
     !$omp target enter data map(to:this%gpoint_flavor)
     call zero_array(ngpt, nlay, ncol, tau)
@@ -802,7 +801,6 @@ contains
     ! Compute internal (Planck) source functions at layers and levels,
     !  which depend on mapping from spectral space that creates k-distribution.
     !$acc enter data copyin(sources)
-    !$omp target enter data map(to:sources)
     !$acc enter data create(sources%lay_source, sources%lev_source_inc, sources%lev_source_dec, sources%sfc_source)
     !$omp target enter data map(alloc:sources%lay_source, sources%lev_source_inc, sources%lev_source_dec, sources%sfc_source)
     !$acc enter data create(sfc_source_t, lay_source_t, lev_source_inc_t, lev_source_dec_t) attach(tlev_wk)
@@ -842,7 +840,6 @@ contains
     !$acc exit data copyout(sources%lay_source, sources%lev_source_inc, sources%lev_source_dec, sources%sfc_source)
     !$omp target exit data map(from:sources%lay_source, sources%lev_source_inc, sources%lev_source_dec, sources%sfc_source)
     !$acc exit data copyout(sources)
-    !$omp target exit data map(from:sources)
   end function source
   !--------------------------------------------------------------------------------------------------------------------
   !
@@ -900,7 +897,6 @@ contains
     character(len = 128) :: err_message
     ! ----
     !$acc enter data create(this)
-    !$omp target enter data map(alloc:this)
     err_message = init_abs_coeffs(this, &
                                   available_gases, &
                                   gas_names, key_species,    &
@@ -1006,7 +1002,6 @@ contains
     integer :: ngpt
     ! ----
     !$acc enter data create(this)
-    !$omp target enter data map(alloc:this)
     err_message = init_abs_coeffs(this, &
                                   available_gases, &
                                   gas_names, key_species,    &
@@ -1758,7 +1753,6 @@ contains
     nlay = size(tau, 2)
     ngpt = size(tau, 1)
     !$acc enter data copyin(optical_props)
-    !$omp target enter data map(to:optical_props)
     if (.not. has_rayleigh) then
       ! index reorder (ngpt, nlay, ncol) -> (ncol,nlay,gpt)
       !$acc enter data copyin(tau)
@@ -1819,7 +1813,6 @@ contains
       !$omp target exit data map(release:tau, tau_rayleigh)
     end if
     !$acc exit data copyout(optical_props)
-    !$omp target exit data map(from:optical_props)
   end subroutine combine_and_reorder
 
   !--------------------------------------------------------------------------------------------------------------------
