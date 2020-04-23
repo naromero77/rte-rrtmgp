f3adfb76c9e91477dfbab50bc25d3db6156d16e2
naromero77 <naromero@anl.gov>
2020-04-23 13:11:09 -0500

    Add missing `omp target` directives.

--- rrtmgp/mo_gas_concentrations.F90
+++ rrtmgp/mo_gas_concentrations.F90
@@ -306,20 +306,22 @@ contains
     if(error_msg /= "") return
 
     !$acc data copyout (array) present(this)
-    !$omp data map(from:array)
+    !$omp target data map(from:array)
     if(size(this%concs(igas)%conc, 2) > 1) then
       !$acc kernels default(none)
+      !$omp target
       array(:) = this%concs(igas)%conc(1,:)
       !$acc end kernels
       !$omp end target
     else
       !$acc kernels default(none)
+      !$omp target
       array(:) = this%concs(igas)%conc(1,1)
       !$acc end kernels
       !$omp end target
     end if
     !$acc end data
-    !$omp end data
+    !$omp end target data
 
   end function get_vmr_1d
   ! -------------------------------------------------------------------------------------
@@ -354,7 +356,7 @@ contains
     if(error_msg /= "") return
 
     !$acc data copyout (array) present(this, this%concs)
-    !$omp data map(from:array)
+    !$omp target data map(from:array)
     if(size(this%concs(igas)%conc, 1) > 1) then      ! Concentration stored as 2D
       !$acc parallel loop collapse(2) default(none)
       do ilay = 1, size(array,2)
@@ -379,7 +381,7 @@ contains
       end do
     end if
     !$acc end data
-    !$omp end data
+    !$omp end target data
 
   end function get_vmr_2d
   ! -------------------------------------------------------------------------------------
